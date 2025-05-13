01e878f6167cf400c4e801c2779979405b1b0583
Anel <anel.imamovic@telekom.de>
2025-05-13 16:58:42 +0200

    [1] new method

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,41 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div id="app">
+      <OrgChart />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+  import OrgChart from './components/OrgChart.vue'
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+  export default {
+      name: 'app',
+      components: {
+          OrgChart,
+      }
+  }
+</script>
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+<style>
+  #app {
+      font-family: 'Avenir', Helvetica, Arial, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      text-align: center;
+      color: #2c3e50;
+      margin-top: 60px;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
+  html, body {
+      width: 100%;
+      height: 100%;
+      padding: 0;
+      margin: 0;
+      overflow: hidden;
+      font-family: Helvetica;
   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  #tree {
+      width: 100%;
+      height: 100%;
   }
-}
 </style>
